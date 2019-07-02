84a021349c57da872747b86cf8b3f9963c9738df
alardin531 <51431718+alardin531@users.noreply.github.com>
2019-07-02 10:38:26 +0800

    gg

--- 未命名1.cpp
+++ 未命名1.cpp
@@ -1,4 +1,19 @@
-#include<stdio.h>
-#define N 20
-main(){int a[N][N];int i,j,k,n;printf("Please input n(0<n<20):");scanf("%d",&n);k=1;for(i=0;i<n/2;i++){   for(j=i;j<n-i-1;j++)    a[i][j]=k++;   for(j=i;j<n-i-1;j++)    a[j][n-i-1]=k++;   for(j=n-i-1;j>i;j--)    a[n-i-1][j]=k++;   for(j=n-i-1;j>i;j--)    a[j][i]=k++;}if(n%2==1)   a[n/2][n/2]=k;for(i=0;i<n;i++){   for(j=0;j<n;j++)    printf("%4d",a[i][j]);   printf("/n");}}
-
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,k,sum=0,a,i=0,j;
+	cin>>n;
+	j=n;
+	while(j>0){
+		j/=10;
+		i++;
+	}
+	for(k=0;k<i;k++){
+		a=n%10;
+		n/=10;
+		sum+=a;
+	}
+	cout<<i<<" "<<sum;
+	return 0;
+ } 
