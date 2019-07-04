7b3344a9b0de98d804b786fa3d1e42ce5c566142
alardin531 <51431718+alardin531@users.noreply.github.com>
2019-07-04 11:42:50 +0800

    jk

--- 未命名1.cpp
+++ 未命名1.cpp
@@ -1,26 +1,61 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-  const char key[ ] = {'a','c','b','a','d'};
-  const int NUM_QUES = 5;
-  char c;
-  int ques = 0, numCorrect = 0;
-  cout << "Enter the " << NUM_QUES << " question tests:" << endl;
-  while(cin.get(c)) {
-    if(c != '\n') {
-         if(c == key[ques]) {
-         numCorrect++; cout << " ";
-       } else
-         cout<<"*";
-       ques++;
-    } else {
-      cout << " Score " << static_cast<float>(numCorrect)/NUM_QUES*100 << "%";
-      ques = 0;  numCorrect = 0; cout << endl;
-    }
-  }
-  if(ques!=0){
-  	cout<<"ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyou are cheated ! "<<endl;
-  } 
-  return 0;
-}
+class Date{
+	public:
+		int y,m,d,sum=0,fsum=0,key;
+	Date(int a,int b,int c): y(a),m(b),d(c){
+		if((y%4==0&&y%100!=0)||(y%400==0)){
+			key=29;
+		}
+		else {key=28;
+		}
+	}
+	void showtime(){
+		int sa[13];
+		if(y==m==d){
+			key=0;
+		}
+		else {
+			if(key==29){
+			for(unsigned i=1;i<13;i++){
+				if(i==1||m==3||i==5||i==7||i==8||i==10||i==12){
+					sa[i]=31;
+			}
+			    else if(i==2){
+			    	sa[i]=key;
+				}
+				else {
+					sa[i]=30;
+				}
+			}
+				
+			}
+		else if(key==28){
+			for(unsigned i=1;i<13;i++){
+				if(i==1||m==3||i==5||i==7||i==8||i==10||i==12){
+					sa[i]=31;
+			}
+			    else if(i==2){
+			    	sa[i]=key;
+				}
+				else {
+					sa[i]=30;
+				}
+			}
+		}
+		for(unsigned i=1;i<=m;i++){
+			sum+=sa[i];
+		}
+		fsum=sum+d;
+		cout<<fsum<<endl;
+		}
 
+		}
+};
+int main(){
+	int j,k,l;
+	cin>>j>>k>>l;
+	Date a(j,k,l);
+	a.showtime();
+	return 0;
+}
